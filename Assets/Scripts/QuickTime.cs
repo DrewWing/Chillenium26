985f13ec0b645a7221a3ec5968a094628a5f49d2
Drew Wingfield <89153952+DrewWing@users.noreply.github.com>
2026-02-21 11:42:47 -0600

    Revert "Fix QuickTime a bit"
    This reverts commit 62b798a2e5e4b670cd96d4d1b8b04564e26a593a.

--- Assets/Scripts/QuickTime.cs
+++ Assets/Scripts/QuickTime.cs
@@ -3,55 +3,27 @@ using UnityEngine;
 // manager for the quick time events
 
 
-public class QuickTime : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    public float timeLeft = 5f; // in sec, time left to hit the keys
-    public float timeMax = 5f;
-    //public const float timeMargin = 0.2f; // time in margin after officially over that we still accept input (TODO: implement logic)
-
-    public float timeStart = 0.0f;
-    public float timeEnd = 0.0f;
+    public int timeCurrent = 5000; // in ms, time left to hit the keys
+    public int timeMax = 5000;
+    public const int timeMargin = 20; // time in margin after officially over that we still accept input (TODO: implement logic)
 
     void Start()
     {
-        // TODO
     }
 
     void keyHit(string key) // when key is hit ("up", "down", "left", "right")
     {
-        // logic TODO: check to see if key is correct
+        // logic: check to see if key is correct
 
         // run other functions to do vfx, sfx, etc.
 
     }
 
-    void startQuickTime(int numbKeys)
+    void runfx(Player player)
     {
-        // creates a quick time event using a number of randomly selected keys.
-        // starts timer.
-        timeStart = timeStart.time;
-        timeEnd = timeStart + timeMax;
-        timeLeft = timeEnd - Time.time;
-        
-        // TODO; create keys
-    }
-
-    void FixedUpdate()
-    {
-        timeLeft = timeEnd - Time.time;
-
-
-        // Time up
-        if (timeLeft <= 0.0f)
-        {
-            endQuickTime();
-            return;
-        }
-
-        // Otherwise, process keys
-
-        // TODO: process keys here.
-
+        // Logic goes here
     }
 
 }
