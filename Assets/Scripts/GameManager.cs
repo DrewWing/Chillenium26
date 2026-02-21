fd5e92fd36fdcf8cc4eaafb0ba8f8c52502bc61b
ruheengl <ruheenagulwar7@gmail.com>
2026-02-21 10:47:00 -0600

    Base project upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,113 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Cover")]
+    public float flashImageDuration = 3.0f;
+
+    public GameObject gameCoverPanel;
+
+    [Header("Main Menu")]
+    public GameObject menuPanel;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (gameCoverPanel != null)
+        StartCoroutine(FlashImage(gameCoverPanel));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyManager();
+    }
+
+    IEnumerator FlashImage(GameObject targetImage)
+    {
+        targetImage.SetActive(true);
+        yield return new WaitForSeconds(flashImageDuration);
+        targetImage.SetActive(false);
+    }
+
+    public void LoadSceneByName(string sceneName)
+    {
+        Debug.Log(sceneName + " loaded");
+        // SceneManager.LoadScene(sceneName);
+    }
+
+    private void KeyManager()
+    {
+        if (Keyboard.current.spaceKey.isPressed)
+        {
+            Debug.Log("Space key was pressed!");
+        }
+        else if (Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("W was pressed!");
+        }
+        else if (Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("A was pressed!");
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("S was pressed!");
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("D was pressed!");
+        }
+        else if (Keyboard.current.upArrowKey.isPressed)
+        {
+            Debug.Log("Up was pressed!");
+        }
+        else if (Keyboard.current.escapeKey.isPressed)
+        {
+            DoExitGame();
+        }
+    }
+
+    public void DoExitGame()
+    {
+        Application.Quit();
+    }
+}
+
+
+
+/*
+    Game Start
+    Title Screen Flash
+    Main Menu
+    - Start Button
+    - Exit button
+    - Options (Volume up down)
+    
+    Start Button
+    - Cut scene (optional)
+    - front of arcade machine screen
+    - WASD keys control, arrow keys control
+    -- W: 
+    -- A: 
+    -- S:
+    -- D: 
+    -- Up: 
+    -- Down:
+    -- Left:
+    -- Right:
+
+    1 player, 3 enemies
+    Player
+    - Health = 5 hitpoints
+    points Reset every level
+
+    hitpoints select damage level with keys
+    quick time event 
+    - win, move to next level 
+*/
